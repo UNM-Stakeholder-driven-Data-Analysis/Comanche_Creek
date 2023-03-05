4e9f663a95a7445ba186e85a432ae2b9488df582
Laurel Martinez <lfmartinez@unm.edu>
2023-03-05 16:06:14 -0700

    added libraries, created a basemap of study site, and added shapefiles of watershed boundaries

--- R_scripts/01_datawrangling.R
+++ R_scripts/01_datawrangling.R
@@ -2,5 +2,36 @@
 
 #### Libraries ####
 
+# install.packages('devtools')
+# devtools::install_github("dkahle/ggmap")
+# install.packages('maps')
+install.packages('rgdal')
+install.packages('rgeos')
+install.packages('raster')
+
+library(rgdal)
+library(rgeos)
+library(raster)
+library(ggmap)
+
 #### Load Data ####
+
+#### Basemap ####
+
+#creating map of Valle Vidal unit on Carson Nat'l Forest
+ValleVidalMap <- get_stamenmap(bbox = c(left = -105.3393,
+                                bottom = 36.7202,
+                                right = -105.2284,
+                                top = 36.8401),
+                       maptype = "terrain", 
+                       crop = FALSE,
+                       zoom = 12)
+ggmap(ValleVidalMap)
+
+#Loading shapefiles of Grassy Creek and No Name Creek watershed boundaries
+GrassyCreekWatershed <- readOGR(dsn = "data/raw/Watershed_Boundaries",
+                               "Watersheds_grassy creek")
+NoNameCreekWatershed <- readOGR(dsn = "data/raw/Watershed_Boundaries",
+                               "Watersheds_no name creek")
+
 
