8a87b812d3a1719a2850cb684f00ddd9e6777ea7
Laurel Martinez <lfmartinez@unm.edu>
2023-03-05 17:55:49 -0700

    Added Landsat data, reprojected watershed shapefiles to UTM

--- R_scripts/01_datawrangling.R
+++ R_scripts/01_datawrangling.R
@@ -16,9 +16,9 @@ library(ggmap)
 
 #### Load Data ####
 
-#### Basemap ####
+#### Mapping ####
 
-#creating map of Valle Vidal unit on Carson Nat'l Forest
+#creating basemap of Valle Vidal unit on Carson Nat'l Forest
 ValleVidalMap <- get_stamenmap(bbox = c(left = -105.3393,
                                 bottom = 36.7202,
                                 right = -105.2284,
@@ -36,8 +36,48 @@ NoNameCreekWatershed <- readOGR(dsn = "data/raw/Watershed_Boundaries",
 #View metadata to get info about object class, coordinate reference system, and spatial extent
 GrassyCreekWatershed
 NoNameCreekWatershed
-  #these shapefiles are polygons using the transverse mercator   map projection 
-#checking out the attributes of the shapefiles
-GrassyCreekWatershed@data
-NoNameCreekWatershed@data
-  #nothing super useful in the attributes
+#these shapefiles are polygons using the transverse mercator  map projection 
+#we need them to be in UTM so they have the same crs as the Landsat 8 data
+
+#Add Landsat data; we need red and infrared bands for NDVI
+
+red <- raster("data/raw/Landsat8_082013/LC08_L1TP_033034_20130809_20200912_02_T1_B4.TIF")
+near.infrared <- raster("data/raw/Landsat8_082013/LC08_L1TP_033034_20130809_20200912_02_T1_B5.TIF")
+
+#reproject the watershed shapefiles using the crs for the Landsat data, in this case we using the 'red' band which uses the UTM projection
+
+GrassyUTM <- spTransform(GrassyCreekWatershed,
+                              crs(red))
+crs(GrassyUTM)
+#now the Grassy Creek Watershed shapfile is in UTM instead of tmerc
+
+NoNameUTM <- spTransform(NoNameCreekWatershed,
+                         crs(red))
+crs(NoNameUTM)
+
+
+# #plot shapefiles
+# plot(ValleVidalMap)
+# plot(NoNameCreekWatershed, col = "green", add = TRUE)
+# plot(GrassyCreekWatershed, col = "blue", add = TRUE)
+#   #I can't get both shapefiles to plot on the same map, so I'm   going to move on for now and come back to this later
+
+
+#plot red band
+plot(red, col = rev(terrain.colors(50)))
+
+#plot Grassy Creek shapefile
+plot(GrassyUTM,
+     main = "Grassy Creek Watershed",
+     axes = TRUE,
+     border = "blue")
+
+# crop the lidar raster using the vector extent
+red_crop_grassy <- crop(red, GrassyUTM)
+
+plot(red_crop_grassy)
+
+# add shapefile on top of the existing raster
+plot(GrassyUTM, add = TRUE)
+
+
