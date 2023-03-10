2a52ec5f3896a8f7b6c6c0429aacb12747b98464
Laurel Martinez <lfmartinez@unm.edu>
2023-03-10 08:59:46 -0700

    organized load data section to tidy data up

--- R_scripts/02_dataexploration.R
+++ R_scripts/02_dataexploration.R
@@ -2,13 +2,68 @@
 
 #### Libraries ####
 install.packages('nortest')
+install.packages("spdep")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("tmap")
+install.packages("shinyjs")
+install.packages('lubridate')
+install.packages('psych') # to plot pair-wise correlations
+install.packages('car') # I like their qq plot fxn
+install.packages('tsibble') # useful for creating time series objects
+install.packages('forecast') # I like their Acf fxn
+install.packages('ape') # for spatial autocorrelation
+install.packages('ade4')# for spatial autocorrelation
+install.packages('rgdal')
+
+#Load libraries
+library(tmap)
+library(spdep)
+library(raster)
+library(shinyjs) 
 library(reshape2)
 library(tidyverse)
 library(nortest)
+library(tidyverse)
+library(lubridate)
+library(psych) # to plot pair-wise correlations
+library(car) # I like their qq plot fxn
+library(tsibble) # useful for creating time series objects
+library(forecast) # I like their Acf fxn
+library(ape) # for spatial autocorrelation
+library(ade4)# for spatial autocorrelation
+library(rgdal) # for mapping
 
 #### Load Data ####
 
 NDVI_Full <- read.csv("data/processed/NDVIGrassyNoName_Full.csv", header = T, stringsAsFactors = T)
+
+#tidy data- make the before and after periods in the dataframe
+#"before" (2005-2013) and "after" (2013-2022)
+
+NDVI_Full_BA <- NDVI_Full %>% 
+  mutate("Period" = case_when(`Year` == "2005" ~ "Before",
+                              `Year` == "2006" ~ "Before",
+                              `Year` == "2008" ~ "Before",
+                              `Year` == "2010" ~ "Before",
+                              `Year` == "2011" ~ "Before",
+                              `Year` == "2012" ~ "Before",
+                              `Year` == "2013" ~ "Before",
+                              `Year` == "2014" ~ "After",
+                              `Year` == "2015" ~ "After",
+                              `Year` == "2016" ~ "After",
+                              `Year` == "2017" ~ "After",
+                              `Year` == "2018" ~ "After",
+                              `Year` == "2019" ~ "After",
+                              `Year` == "2020" ~ "After",
+                              `Year` == "2021" ~ "After",
+                              `Year` == "2022" ~ "After"))
+#convert Period from character to factor
+NDVI_Full_BA$Period <- as.factor(NDVI_Full_BA$Period)
+class(NDVI_Full_BA$Period)
+
+summary(NDVI_Full_BA)
+
 
 #### Data Types ####
 dim(NDVI_Full)
@@ -35,32 +90,9 @@ NDVI_Full %>% ggplot(aes(x=NDVI)) +
               facet_wrap(~Site) +
   labs(title="NDVI Histogram",x="NDVI", y = "Number of observations")
 
-#let's also see what it's like in a "before" (2005-2013) and "after" period (2013-2022)
-#first we have to make the before and after periods in the dataframe
-NDVI_Full_BA <- NDVI_Full %>% 
-  mutate("Period" = case_when(`Year` == "2005" ~ "Before",
-                              `Year` == "2006" ~ "Before",
-                              `Year` == "2008" ~ "Before",
-                              `Year` == "2010" ~ "Before",
-                              `Year` == "2011" ~ "Before",
-                              `Year` == "2012" ~ "Before",
-                              `Year` == "2013" ~ "Before",
-                              `Year` == "2014" ~ "After",
-                              `Year` == "2015" ~ "After",
-                              `Year` == "2016" ~ "After",
-                              `Year` == "2017" ~ "After",
-                              `Year` == "2018" ~ "After",
-                              `Year` == "2019" ~ "After",
-                              `Year` == "2020" ~ "After",
-                              `Year` == "2021" ~ "After",
-                              `Year` == "2022" ~ "After"))
-                              
-summary(NDVI_Full_BA)
 
-#next step- convert Period from character to factor
 
-NDVI_Full_BA$Period <- as.factor(NDVI_Full_BA$Period)
-class(NDVI_Full_BA$Period)
+
 
 #Histograms of data Before vs After
 NDVI_Full_BA %>% mutate(Period = fct_relevel(Period, 
@@ -80,4 +112,8 @@ NDVI_Full_BA %>% mutate(Period = fct_relevel(Period,
 #
 
 
+
+
+#### Data Structure and Relationships ####
+#testing for autocorrelation
 
