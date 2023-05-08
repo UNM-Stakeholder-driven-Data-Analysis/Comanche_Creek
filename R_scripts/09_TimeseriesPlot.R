2681a683d6f75ed558188c7a860dcd1aa3ffac9d
Laurel Martinez <lfmartinez@unm.edu>
2023-05-08 16:18:33 -0600

    Created a new file to make the time plot

---
+++ R_scripts/09_TimeseriesPlot.R
@@ -0,0 +1,9 @@
+#### Read Me ####
+#The purpose of this code is to create a plot to visualize how NDVI is changing over time in the control and impact reaches during the study period at both the full watershed and buffered wetland area levels
+
+#### Libraries ####
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+#### Load Data ####
